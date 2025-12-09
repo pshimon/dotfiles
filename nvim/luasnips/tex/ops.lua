4879ff558df79126f2b90008041a18a0fbd94536
Shimon Panfil <shimon.panfil@gmail.com>
2025-12-09 18:13:03 +0200

    remove collisions

--- nvim/luasnips/tex/ops.lua
+++ nvim/luasnips/tex/ops.lua
@@ -6,16 +6,16 @@ return {
 	-- Examples of math operator snippets, autotriggered for efficiency
 	-- not all greek letters exit in math mode
 	-- latin ones used for absent
-	s({ trig = ";ap", snippetType = "autosnippet" }, {
+	s({ trig = ",ap", snippetType = "autosnippet" }, {
 		t("\\approx"),
 	}),
 	s({ trig = ",p", snippetType = "autosnippet" }, {
 		t("\\partial"),
 	}),
-	s({ trig = ";ra", snippetType = "autosnippet" }, {
+	s({ trig = ",ra", snippetType = "autosnippet" }, {
 		t("\\rightarrow"),
 	}),
-	s({ trig = ";la", snippetType = "autosnippet" }, {
+	s({ trig = ",la", snippetType = "autosnippet" }, {
 		t("\\leftarrow"),
 	}),
 	s({ trig = ",t", snippetType = "autosnippet" }, {
